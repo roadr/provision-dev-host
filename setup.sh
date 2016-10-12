c903a1cd873646c63d220f6fa5b9e7a207ea71a5
Ivo Senese <ivo.senese@localhost.localdomain>
2016-10-12 16:09:13 +0200

    Adding dotfiles to provisioning scripts.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
+echo "Installing basics to run ansible playbooks"
 sudo dnf --releasever=24 -y install \
   ansible-2.1.2.0 \
   python2 \
   python2-dnf
+
+echo "Downloading provision-dev-host scripts"
 curl https://codeload.github.com/roadr/provision-dev-host/zip/master > /tmp/provision-dev-host.zip
 unzip /tmp/provision-dev-host.zip -d /tmp
 cd /tmp/provision-dev-host-master
