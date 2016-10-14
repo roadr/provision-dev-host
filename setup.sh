6b260eb98ee1fe618d7f31af065c9acfcde2a3e6
Ivo Senese <roadr@users.noreply.github.com>
2016-10-14 17:18:43 +0200

    Added python-selinux bindings

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ echo "Installing basics to run ansible playbooks"
 sudo dnf --releasever=24 -y install \
   ansible-2.1.2.0 \
   python2 \
-  python2-dnf
+  python2-dnf \
+  libselinux-python
 
 echo "Downloading provision-dev-host scripts"
 curl https://codeload.github.com/roadr/provision-dev-host/zip/master > /tmp/provision-dev-host.zip
