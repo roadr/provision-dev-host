4fea1862f8b6638b87bef5dcc5d0de88ccca8fdb
Ivo Senese <>
2018-09-28 10:34:06 +0200

    Updated fedora release version, added some missing libraries for running ansible.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 echo "Installing basics to run ansible playbooks"
-sudo dnf --releasever=27 -y install \
-  ansible-2.6.2 \
+sudo dnf --releasever=28 -y install \
+  ansible-2.6.4 \
   python2 \
+  python2-requests \
   python2-dnf
 
 echo "Downloading provision-dev-host scripts"
-curl https://codeload.github.com/roadr/provision-dev-host/zip/master > /tmp/provision-dev-host.zip
+curl https://codeload.github.com/roadr/provision-dev-host/zip/java-update > /tmp/provision-dev-host.zip
 unzip /tmp/provision-dev-host.zip -d /tmp
-cd /tmp/provision-dev-host-master
+cd /tmp/provision-dev-host-java-update
 ansible-playbook -i hosts.yaml dev-machine.yaml -K
